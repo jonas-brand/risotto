1f3cbb40d420f43e439d46a1349407a895d152b6
jonas-brand <jonas.brand@bluewin.ch>
2024-03-10 19:14:15 +0100

    add sway to setup script

--- setup.sh
+++ setup.sh
@@ -12,9 +12,11 @@ unlink "$XDG_CONFIG_HOME/gtk-2.0"
 ln -sf "$projdir/config/gtk-2.0" "$XDG_CONFIG_HOME/gtk-2.0"
 unlink "$XDG_CONFIG_HOME/gtk-3.0"
 ln -sf "$projdir/config/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
-unlink "$XDG_CONFIG_HOME/hypr"
-ln -sf "$projdir/config/hypr" "$XDG_CONFIG_HOME/hypr"
 unlink "$XDG_CONFIG_HOME/nvim"
 ln -sf "$projdir/config/nvim" "$XDG_CONFIG_HOME/nvim"
 unlink "$XDG_CONFIG_HOME/zsh"
 ln -sf "$projdir/config/zsh" "$XDG_CONFIG_HOME/zsh"
+unlink "$XDG_CONFIG_HOME/hypr"
+ln -sf "$projdir/config/sway" "$XDG_CONFIG_HOME/sway"
+
+reboot
